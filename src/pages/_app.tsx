4b75d9bf3b26a860ec570144bf750db43427b94b
Kazushige Kobayashi <kobayashi@stmn.co.jp>
2023-03-11 22:22:08 +0900

    npm install swr

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -26,20 +26,17 @@ const GlobalStyle = createGlobalStyle`
   }
 `
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
-  return (
-      <>
-        <Head>
-          <meta key="charset" name="charset" content="utf-8" />
-          <meta
-              key="viewport"
-              name="viewport"
-              content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
-          />
-        </Head>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </>
-  )
-}
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <meta key="charset" name="charset" content="utf-8" />
+      <meta
+        key="viewport"
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5" />
+    </Head>
+    <GlobalStyle />
+    <Component {...pageProps} />
+  </>
+)
 export default MyApp

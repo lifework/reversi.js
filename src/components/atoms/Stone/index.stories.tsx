80e7e14fddac0eed48f85d169269c0c9b2c37bf5
Kazushige Kobayashi <kobayashi@stmn.co.jp>
2023-03-21 01:02:29 +0900

    Stone プロトタイプ

--- src/components/atoms/Stone/index.stories.tsx
+++ src/components/atoms/Stone/index.stories.tsx
@@ -1,5 +1,24 @@
+import CircleIcon from '@mui/icons-material/Circle'
+import CircleOutlinedIcon from '@mui/icons-material/CircleOutlined'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 import Stone from './index'
+
+type CircleIconProps = {
+  fontSize?: string
+  color?: string
+}
+
+const CircleIconComponent = ({ fontSize, color }: CircleIconProps) => (
+  <span style={{ fontSize, color }}>
+    <CircleIcon fontSize="inherit" color="inherit" />
+  </span>
+)
+
+const CircleOutlinedIconComponent = ({ fontSize, color }: CircleIconProps) => (
+  <span style={{ fontSize, color }}>
+    <CircleOutlinedIcon fontSize="inherit" color="inherit" />
+  </span>
+)
 
 export default {
   title: 'Atoms/Stone',
@@ -8,23 +27,15 @@ export default {
       options: ['black', 'white'],
       control: { type: 'radio' },
       defaultValue: 'black',
-      // docsに表示する内容を設定
-      description: 'Stone Variant',
+      description: 'ボタンバリアント',
       table: {
-        type: { summary: 'white | black' },
+        type: { summary: 'black | white' },
         defaultValue: { summary: 'black' },
       },
     },
-    width: {
+    size: {
       control: { type: 'number' },
-      description: '石の横幅',
-      table: {
-        type: { summary: 'number' },
-      },
-    },
-    height: {
-      control: { type: 'number' },
-      description: '石の縦幅',
+      description: '石の大きさ',
       table: {
         type: { summary: 'number' },
       },
@@ -36,8 +47,14 @@ const Template: ComponentStory<typeof Stone> = (args) => <Stone {...args} />
 
 // Black
 export const Black = Template.bind({})
-Black.args = { variant: 'black', children: 'Black Stone' }
+Black.args = {
+  variant: 'black',
+  children: <CircleIconComponent color="black" />,
+}
 
 // White
 export const White = Template.bind({})
-White.args = { variant: 'white', children: 'White Stone' }
+White.args = {
+  variant: 'white',
+  children: <CircleOutlinedIconComponent color="black" />,
+}

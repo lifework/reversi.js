ee4f75731ac22f09720c3cd7b85e052a001999ac
Kazushige Kobayashi <kobayashi@stmn.co.jp>
2023-03-22 21:42:18 +0900

    Disk を FC で書き直し

--- src/components/atoms/Disk/index.stories.tsx
+++ src/components/atoms/Disk/index.stories.tsx
@@ -1,45 +1,9 @@
-import { Circle, CircleOutlined } from '@mui/icons-material'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
-import Disk from './index'
-
-type CircleIconProps = {
-  fontSize?: string
-  color?: string
-}
-
-const CircleIconComponent = ({ fontSize, color }: CircleIconProps) => (
-  <span style={{ fontSize, color }}>
-    <Circle fontSize="inherit" color="inherit" />
-  </span>
-)
-
-const CircleOutlinedIconComponent = ({ fontSize, color }: CircleIconProps) => (
-  <span style={{ fontSize, color }}>
-    <CircleOutlined fontSize="inherit" color="inherit" />
-  </span>
-)
+import { Disk } from './index'
 
 export default {
   title: 'Atoms/Disk',
-  argTypes: {
-    variant: {
-      options: ['black', 'white'],
-      control: { type: 'radio' },
-      defaultValue: 'black',
-      description: 'バリアント',
-      table: {
-        type: { summary: 'black | white' },
-        defaultValue: { summary: 'black' },
-      },
-    },
-    size: {
-      control: { type: 'number' },
-      description: '大きさ',
-      table: {
-        type: { summary: 'number' },
-      },
-    },
-  },
+  component: Disk,
 } as ComponentMeta<typeof Disk>
 
 const Template: ComponentStory<typeof Disk> = (args) => <Disk {...args} />
@@ -47,13 +11,11 @@ const Template: ComponentStory<typeof Disk> = (args) => <Disk {...args} />
 // Black
 export const Black = Template.bind({})
 Black.args = {
-  variant: 'black',
-  children: <CircleIconComponent color="black" />,
+  color: 'black',
 }
 
 // White
 export const White = Template.bind({})
 White.args = {
-  variant: 'white',
-  children: <CircleOutlinedIconComponent color="black" />,
+  color: 'white',
 }
--- src/components/atoms/Disk/index.tsx
+++ src/components/atoms/Disk/index.tsx
@@ -1,54 +1,18 @@
-/* eslint-disable prettier/prettier */
-import styled from 'styled-components'
-import { Responsive } from 'types'
-import { toPropValue, FontSize, Space } from 'utils/styles'
+import { Circle, CircleOutlined } from '@mui/icons-material'
+import { FC, ComponentProps } from 'react'
 
-// ボタンのバリアント
-export type DiskVariant = 'black' | 'white'
+export type DiskColor = 'black' | 'white'
+export type DiskSize = '16px' | '24px'
 
 export type DiskProps = {
-  variant?: DiskVariant
-  fontSize?: Responsive<FontSize>
-  margin?: Responsive<Space>
-  padding?: Responsive<Space>
+  color: DiskColor
+  size: ComponentProps<typeof Circle>['fontSize']
 }
 
-const variants = {
-  black: {
-    color: 'black',
-    fontSize: 'extraLarge',
-  },
-  white: {
-    color: 'white',
-    fontSize: 'extraLarge',
-  },
+export const Disk: FC<DiskProps> = ({ color, size }) => {
+  return color === 'black' ? (
+    <Circle fontSize={size} color="inherit" />
+  ) : (
+    <CircleOutlined fontSize={size} color="inherit" />
+  )
 }
-
-/**
- * ボタン
- * バリアント、色、タイポグラフィ、レイアウト、スペース関連のPropsを追加
- */
-const Disk = styled.span<DiskProps>`
-  ${({ variant, color, fontSize, theme }) => {
-    // バリアントのスタイルの適用
-    if (variant && variants[variant]) {
-      const styles = []
-      !color && styles.push(toPropValue('color', variants[variant].color, theme))
-
-      !fontSize && styles.push(toPropValue('font-size', variants[variant].fontSize, theme))
-      return styles.join('\n')
-    }
-  }}
-  ${(props) => toPropValue('color', props.color, props.theme)}
-  ${(props) => toPropValue('font-size', props.fontSize, props.theme)}
-  ${(props) => toPropValue('margin', props.margin, props.theme)}
-  ${(props) => toPropValue('padding', props.padding, props.theme)}
-`
-
-Disk.defaultProps = {
-  variant: 'black',
-  color: 'black',
-  fontSize: 'extraLarge',
-}
-
-export default Disk
